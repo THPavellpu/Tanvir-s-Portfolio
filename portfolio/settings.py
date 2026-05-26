2bd83afbc045ef4ffa1ec626fb85786416641028
Thpavel <tanvirhossenpavel2018@gmail.com>
2026-05-27 00:12:18 +0530

    Tried Localhost

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -62,9 +62,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         os.environ.get("DATABASE_URL")
+#     )
+# }
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+        config(
+            'DATABASE_URL',
+            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        )
     )
 }
 
